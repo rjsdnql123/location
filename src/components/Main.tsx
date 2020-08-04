216bccbabef8fb0e4af3ecebd2a1b088190005a9
won <rjsdnql123@gmail.com>
2020-08-04 23:53:43 +0900

    Add MainPost in RefreshControl

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,17 @@
-import { TextInput, View, Text, TouchableOpacity } from 'react-native';
-import React, { Component } from 'react';
+import {
+	TextInput,
+	View,
+	Text,
+	TouchableOpacity,
+	ScrollView,
+	RefreshControl,
+	StyleSheet,
+	SafeAreaView,
+} from 'react-native';
+import React, { Component, useState, useEffect } from 'react';
 import styled from 'styled-components/native';
-import {User, MainProps, MainPostData} from '../reducer/type'
-import * as Location from 'expo-location'
+import { User, MainProps, MainPostData } from '../reducer/type';
+import * as Location from 'expo-location';
 const axios = require('axios');
 import { AsyncStorage } from 'react-native';
 import { connect } from 'react-redux';
@@ -11,91 +20,219 @@ import PostDetail from './PostDetail';
 
 //해당 유저의 지역에 걸맞는 포스트 내용들을 불러와준다.
 
-class Main extends Component<MainProps> {
-    constructor(props:MainProps) {
-        super(props)
-    }
+//새로고침 할수 있는 함수
 
-    componentDidMount() {
-        this.props.postSet()
-    }
-    
-    render() {
-        console.log(this.props,'mainProps')
-        if(this.props.post[0] === undefined) {
-            return(
-                <View>
-                    <TouchableOpacity onPress={() => {
-                    return this.props.navigation.navigate('Post')
-                }}><Text>POST쓰기</Text></TouchableOpacity>
-                    <TouchableOpacity onPress={() => {
-                    AsyncStorage.clear();
-                    this.props.isLogin('false')
-    
-                    alert('로그아웃');
-                    // this.props.navigation.navigate('Login'); // 작동됨
-                }}>
-                    <Text>main</Text>
-                </TouchableOpacity> 
-                </View>
-            )
-        }
-        return(
-            <View>
-                <TouchableOpacity onPress={() => {
-                    AsyncStorage.clear();
-                    this.props.isLogin('false')
-    
-                    alert('로그아웃');
-                    // this.props.navigation.navigate('Login'); // 작동됨
-                }}>
-                    <Text>main</Text>
-                </TouchableOpacity> 
-                {this.props.post.map((a, index) => (
-                   <Post key={index}>
-                       <TouchableOpacity onPress={() => this.props.navigation.navigate('PostDetail', {
-                           postIndex: index,
-                           postId: a.post_Id 
-                       })}>
+function Main(props: MainProps) {
+	const wait = (timeout:number) => {
+		return new Promise((resolve) => {
+			setTimeout(resolve, timeout);
+		});
+	};
 
-                    <PostNickname>nickname: {a.user.nickname}</PostNickname>
-                    <Title>title: {a.title}</Title>
-                    <Contents>Contents: {a.contents}</Contents>
-                    <Comments>댓글 몇개?{a.comments.length}</Comments>
-                       </TouchableOpacity>
-                   </Post>
-                ))}
-                <TouchableOpacity onPress={() => {
-                    return this.props.navigation.navigate('Post')
-                }}><Text>POST쓰기</Text></TouchableOpacity>
-            </View>
-        )
-    }
+	const [refreshing, setRefreshing] = React.useState(false);
+
+	const onRefresh = React.useCallback(() => {
+		setRefreshing(true);
+
+		wait(1000).then(() => setRefreshing(false));
+	}, []);
+
+	useEffect(() => {
+		console.log(props, 'Main');
+		const { userId } = props.route.params;
+		if (userId) {
+			props.postSet(userId);
+		}
+	}, []);
+
+	console.log(props, 'mainProps');
+	if (props.post[0] === undefined) {
+		return (
+			<PostView>
+				<TouchableOpacity
+					onPress={() => {
+						return props.navigation.navigate('Post');
+					}}
+				>
+					<Text>POST쓰기</Text>
+				</TouchableOpacity>
+				<TouchableOpacity
+					onPress={() => {
+						AsyncStorage.clear();
+						props.isLogin('false');
+
+						alert('로그아웃');
+					}}
+				>
+					<Text>main</Text>
+				</TouchableOpacity>
+			</PostView>
+		);
+	}
+	return (
+		<PostView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+			{props.post.map((a, index) => (
+				<Post key={index}>
+					<TouchableOpacity
+						onPress={() =>
+							props.navigation.navigate('PostDetail', {
+								postIndex: index,
+								postId: a.post_Id,
+							})
+						}
+					>
+						<PostNickname>nickname: {a.user.nickname}</PostNickname>
+						<Title>title: {a.title}</Title>
+						<Contents>Contents: {a.contents}</Contents>
+						<Comments>댓글 몇개?{a.comments.length}</Comments>
+					</TouchableOpacity>
+				</Post>
+			))}
+			<TouchableOpacity
+				onPress={() => {
+					return props.navigation.navigate('Post');
+				}}
+			>
+				<Text>POST쓰기</Text>
+			</TouchableOpacity>
+			<TouchableOpacity
+				onPress={() => {
+					AsyncStorage.clear();
+					props.isLogin('false');
+
+					alert('로그아웃');
+					// props.navigation.navigate('Login'); // 작동됨
+				}}
+			>
+				<Text>main</Text>
+			</TouchableOpacity>
+		</PostView>
+	);
 }
 
 //게시글을 터치로 잡아서 해당 게시글 클릭시 해당 게시글 자세히 보기로 이동
-export default Main
-
+export default Main;
+const PostView = styled.ScrollView`
+	margin: 5%;
+`;
 const Post = styled.View`
-   border-bottom-width : 1px;
-   border-bottom-color : #aaa;
-   border-top-width : 1px;
-   border-top-color : #aaa;
+	border-bottom-width: 1px;
+	border-bottom-color: palevioletred;
+	border-top-width: 1px;
+	border-top-color: palevioletred;
 `;
 
-const Title = styled.Text`
-   
-`;
+const Title = styled.Text``;
 
-const Contents = styled.Text`
-   
-`;
+const Contents = styled.Text``;
 
-const PostNickname = styled.Text`
+const PostNickname = styled.Text``;
 
-`;
+const Comments = styled.Text``;
 
-const Comments = styled.Text`
+// import { TextInput, View, Text, TouchableOpacity, ScrollView } from 'react-native';
+// import React, { Component } from 'react';
+// import styled from 'styled-components/native';
+// import {User, MainProps, MainPostData} from '../reducer/type'
+// import * as Location from 'expo-location'
+// const axios = require('axios');
+// import { AsyncStorage } from 'react-native';
+// import { connect } from 'react-redux';
+// import { setLogin } from '../action';
+// import PostDetail from './PostDetail';
 
-`;
+// //해당 유저의 지역에 걸맞는 포스트 내용들을 불러와준다.
 
+// class Main extends Component<MainProps> {
+//     constructor(props:MainProps) {
+//         super(props)
+//     }
+
+//     componentDidMount() {
+//         const { userId } = this.props.route?.params
+//         console.log(userId,'이거뭐여요요ㅛ')
+//         if(userId){
+//             this.props.postSet(userId)
+//         }
+//     }
+
+//     render() {
+//         console.log(this.props,'mainProps')
+//         if(this.props.post[0] === undefined) {
+//             return(
+//                 <PostView>
+//                     <TouchableOpacity onPress={() => {
+//                     return this.props.navigation.navigate('Post')
+//                 }}><Text>POST쓰기</Text></TouchableOpacity>
+//                     <TouchableOpacity onPress={() => {
+//                     AsyncStorage.clear();
+//                     this.props.isLogin('false')
+
+//                     alert('로그아웃');
+//                     // this.props.navigation.navigate('Login'); // 작동됨
+//                 }}>
+//                     <Text>main</Text>
+//                 </TouchableOpacity>
+//                 </PostView>
+//             )
+//         }
+//         return(
+//             <PostView>
+//                 {this.props.post.map((a, index) => (
+//                    <Post key={index}>
+//                        <TouchableOpacity onPress={() => this.props.navigation.navigate('PostDetail', {
+//                            postIndex: index,
+//                            postId: a.post_Id
+//                        })}>
+
+//                     <PostNickname>nickname: {a.user.nickname}</PostNickname>
+//                     <Title>title: {a.title}</Title>
+//                     <Contents>Contents: {a.contents}</Contents>
+//                     <Comments>댓글 몇개?{a.comments.length}</Comments>
+//                        </TouchableOpacity>
+//                    </Post>
+//                 ))}
+//                 <TouchableOpacity onPress={() => {
+//                     return this.props.navigation.navigate('Post')
+//                 }}><Text>POST쓰기</Text></TouchableOpacity>
+//                 <TouchableOpacity onPress={() => {
+//                     AsyncStorage.clear();
+//                     this.props.isLogin('false')
+
+//                     alert('로그아웃');
+//                     // this.props.navigation.navigate('Login'); // 작동됨
+//                 }}>
+//                     <Text>main</Text>
+//                 </TouchableOpacity>
+//             </PostView>
+//         )
+//     }
+// }
+
+// //게시글을 터치로 잡아서 해당 게시글 클릭시 해당 게시글 자세히 보기로 이동
+// export default Main
+// const PostView = styled.ScrollView`
+//    margin: 5%;
+// `;
+// const Post = styled.View`
+//    border-bottom-width : 1px;
+//    border-bottom-color : palevioletred;
+//    border-top-width : 1px;
+//    border-top-color : palevioletred;
+// `;
+
+// const Title = styled.Text`
+
+// `;
+
+// const Contents = styled.Text`
+
+// `;
+
+// const PostNickname = styled.Text`
+
+// `;
+
+// const Comments = styled.Text`
+
+// `;
--- src/containers/Main.ts
+++ src/containers/Main.ts
@@ -1,22 +1,26 @@
-import { Posts, ReduxState, MainPostData } from "../reducer/type";
-import { setPost, setLogin } from "../action";
-import { connect } from "react-redux";
-import Main from "../components/Main";
+import { Posts, ReduxState, MainPostData } from '../reducer/type';
+import { setPost, setLogin } from '../action';
+import { connect } from 'react-redux';
+import Main from '../components/Main';
 import axios from 'axios';
+import * as config from '../../env';
 
-const mapStateToProps = (state:ReduxState) => {
-    console.log(state,'state')
+const mapStateToProps = (state: ReduxState) => {
+	console.log(state, 'state');
 	return {
-	  post: state.reducer.post,
-	  };
-  };
-  const mapDispatchToProps = (dispatch:Function) => {
-      return {
-          isLogin: (userLogin:string) => dispatch(setLogin(userLogin)),
-          postSet: () => axios.get('http://localhost:8080/post/allpost', {
-        }).then(({data}:MainPostData) => {
-            dispatch(setPost(data))
-        })
-      }
-  }
-  export default connect(mapStateToProps, mapDispatchToProps)(Main);
+		post: state.reducer.post,
+	};
+};
+const mapDispatchToProps = (dispatch: Function) => {
+	return {
+		isLogin: (userLogin: string) => dispatch(setLogin(userLogin)),
+		postSet: (userId: number) =>
+			axios
+				.get(`http://${config.SERVER_PORT}/post/location_Post?userId=${userId}`, {})
+				.then(({ data }: MainPostData) => {
+					console.log(data, '이거는 이제 언제 넘어가서 바뀔까');
+					dispatch(setPost(data));
+				}),
+	};
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
