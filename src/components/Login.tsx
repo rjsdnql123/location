6e98c26545b442d4b1d20fd7eace3123b8af4dac
won <rjsdnql123@gmail.com>
2020-08-04 23:57:29 +0900

    Fix Login page style

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,18 +1,19 @@
 import { TextInput, View, Text, TouchableOpacity } from 'react-native';
 import React, { Component } from 'react';
 import styled from 'styled-components/native';
-import {User, LoginProps} from '../reducer/type'
-import axios from 'axios'
+import { User, LoginProps } from '../reducer/type';
+import axios from 'axios';
 import { AsyncStorage } from 'react-native';
-import App from '../App'
+import App from '../App';
 import { connect } from 'react-redux';
 import { setLogin } from '../action';
 import * as config from '../../env';
-import AuthLoding from '../containers/AuthLoding'
+import AuthLoding from '../containers/AuthLoding';
+import { AntDesign, SimpleLineIcons } from '@expo/vector-icons';
 // 값이 바뀔때 마다 이메일 형식인지 확인한다?
 
-class Login extends Component<LoginProps,User> {
-	constructor(props:LoginProps) {
+class Login extends Component<LoginProps, User> {
+	constructor(props: LoginProps) {
 		super(props);
 		this.state = {
 			email: '',
@@ -21,65 +22,119 @@ class Login extends Component<LoginProps,User> {
 	}
 
 	// 이메일 형식인지 확인
-	CheckEmail =  (email:string) => {
+	CheckEmail = (email: string) => {
 		try {
-		const emailRegex = /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
-		const checkEmail = emailRegex.test(email);
-		if (!checkEmail) {
-			return alert('이메일 에러');
-		}
-		axios.post(`http://${config.SERVER_PORT}/user/signin`, {
-			email: this.state.email,
-			password: this.state.password,
-		}).then(async res => {
-			console.log(res)
-			if(res.status === 200) {
-				//성공시 메인 페이지
-				console.log(res,'resresres')
-				await AsyncStorage.setItem('USERTOKEN', res.data.accessToken);
-				this.props.information(res.data.userId)
-				this.props.isLogin('true')
-				return alert('로그인 성공')
-			} else if(res.status === 404){
-				console.log('ddnc')
+			const emailRegex = /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+			const checkEmail = emailRegex.test(email);
+			if (!checkEmail) {
+				return alert('이메일 형식이 아닙니다!');
 			}
-		})
-	} catch (error) {
-		console.log('error')
-		return alert('실패');
-	}
+			axios
+				.post(`http://${config.SERVER_PORT}/user/signin`, {
+					email: this.state.email,
+					password: this.state.password,
+				})
+				.then(async (res) => {
+					if (res.status === 200) {
+						//성공시 메인 페이지
+						await AsyncStorage.setItem('USERTOKEN', res.data.accessToken);
+						this.props.information({ user_Id: res.data.userId });
+						this.props.isLogin('true');
+						return alert('로그인 성공');
+					} else if (res.status === 404) {
+						console.log('ddnc');
+					}
+				});
+		} catch (error) {
+			console.log('error');
+			return alert('실패');
+		}
 	};
 
 	render() {
-		AuthLoding
-		console.log(this.props,'thisporposdia')
+		console.log(this.props, 'thisporposdia');
 		return (
 			<View>
+				<MainLoginText>Log In</MainLoginText>
 				<Email>
-					<Text>email</Text>
-					<TextInput value={this.state.email} onChangeText={(email) => this.setState({ email })} />
+					<AntDesign name="user" size={34} color="black" />
+					<UserEmail
+						placeholder="ID"
+						value={this.state.email}
+						onChangeText={(email: string) => this.setState({ email })}
+					/>
 				</Email>
 				<Email>
-					<Text>password</Text>
-					<TextInput secureTextEntry value={this.state.password} onChangeText={(password) => this.setState({ password })} />
+					<SimpleLineIcons name="lock" size={34} color="black" />
+					<UserEmail
+						secureTextEntry
+						placeholder="password"
+						value={this.state.password}
+						onChangeText={(password: string) => this.setState({ password })}
+					/>
 				</Email>
-				<UserLogin onPress={() => this.CheckEmail(this.state.email)}>
-					<Text>Pick a photo</Text>
-				</UserLogin>
-				<UserLogin onPress={() => this.props.navigation.navigate('SignUp')}>
-					<Text>여기는 회원가입</Text>
-				</UserLogin>
+				<LoginButton onPress={() => this.CheckEmail(this.state.email)}>
+					<LoginText>Log IN</LoginText>
+				</LoginButton>
+				<SignUpView>
+					<SignUpText>이메일이 없습니까? </SignUpText>
+					<TouchableOpacity onPress={() => this.props.navigation.navigate('SignUp')}>
+						<SignUpButton>SIGN UP</SignUpButton>
+					</TouchableOpacity>
+				</SignUpView>
 			</View>
 		);
 	}
 }
 // onPress={() => navigation.navigate('Details')}
 const Email = styled.View`
+	border-bottom-width: 3px;
+	border-bottom-color: palevioletred;
+	display: flex;
+	flex-direction: row;
+	margin: 5%;
+`;
+
+const UserEmail = styled.TextInput`
+	width: 91%;
+`;
+
+const SignUpButton = styled.Text`
+	color: palevioletred;
+`;
+const MainLoginText = styled.Text`
+	margin: 20%;
+	font-size: 30px;
+	font-weight: 900
+	color: palevioletred;
+`;
+
+const LoginButton = styled.TouchableOpacity`
 	border: 1px solid palevioletred;
+	border-radius: 50px;
+	background-color: palevioletred;
+	width: 70%
+	height: 15%
+	justifyContent: center
+    alignItems: center
+	margin: 15%
 `;
 
-const UserLogin = styled.TouchableOpacity`
-	background-color: blue;
+const LoginText = styled.Text`
+	font-size: 30px;
+	font-weight: 900;
+	justifycontent: center;
+	color: white;
 `;
 
-  export default Login
+const SignUpText = styled.Text`
+	color: black;
+`;
+const SignUpView = styled.View`
+	display: flex;
+	flex-direction: row;
+	justifyContent: center
+    alignItems: center
+`;
+
+export default Login;
--- src/containers/Login.ts
+++ src/containers/Login.ts
@@ -1,20 +1,19 @@
-import { ReduxState, Information } from "../reducer/type";
-import { setLogin, setInformation } from "../action";
-import { connect } from "react-redux";
-import Login from "../components/Login";
+import { ReduxState, Information } from '../reducer/type';
+import { setLogin, setInformation } from '../action';
+import { connect } from 'react-redux';
+import Login from '../components/Login';
 
-const mapStateToProps = (state:ReduxState) => {
-	console.log(state,'이거처리 porps')
+const mapStateToProps = (state: ReduxState) => {
+	console.log(state, '이거처리 porps');
 	return {
-	  setLogin: state.reducer.setLogin,
-	  };
-  };
-  const mapDispatchToProps = (dispatch:Function) => {
-	console.log(dispatch,'=====')
-	  return {
-		isLogin: (userLogin:string) => dispatch(setLogin(userLogin)),
-		information: (userInformation:Information) => dispatch(setInformation(userInformation))
-
-	  }
-  }
-  export default connect(mapStateToProps,mapDispatchToProps)(Login);
+		setLogin: state.reducer.setLogin,
+	};
+};
+const mapDispatchToProps = (dispatch: Function) => {
+	console.log(dispatch, '=====');
+	return {
+		isLogin: (userLogin: string) => dispatch(setLogin(userLogin)),
+		information: (userInformation: Information) => dispatch(setInformation(userInformation)),
+	};
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
