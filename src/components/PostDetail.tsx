1ed8bf15924269941eeb5ac6ecc87d02e75e2ed6
won <rjsdnql123@gmail.com>
2020-08-14 23:35:14 +0900

    Fix PostDeatail in memozation

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,46 +1,57 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { TextInput, View, Text, TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native';
 import { PostDetailProps, PostDetailState } from '../reducer/type';
 
 //해당 게시글에 상제 정보 타이틀과 댓글목록
-class PostDetail extends Component<PostDetailProps, PostDetailState> {
-	constructor(props: PostDetailProps) {
-		super(props);
-		this.state = {
-			comments: '',
-		};
-	}
-	componentDidMount() {
-		console.log('재랜더링');
-	}
-	render() {
-		console.log(this.props, '실행');
-		return (
-			<PostScreen>
-				<Post>
-					<Title> title {this.props.post[this.props.route.params.postIndex].title}</Title>
-					<PostNickname>닉네임 대전 {this.props.post[this.props.route.params.postIndex].user.nickname}</PostNickname>
-					<Time> 작성시간 {this.props.post[this.props.route.params.postIndex].updatedAt}</Time>
-					<Title>글 내용 {this.props.post[this.props.route.params.postIndex].contents}</Title>
-					<TextIN>
-						<TextInput value={this.state.comments} onChangeText={(comments) => this.setState({ comments })}></TextInput>
-					</TextIN>
-				</Post>
-				{this.props.post[this.props.route.params.postIndex].comments.map((comments: any, index) => (
-					<Text key={index}>{comments.Contents}</Text>
-				))}
-				<TouchableOpacity
-					onPress={() => (
-						this.props.writingComment(this.props.userId, this.props.route.params.postId, this.state.comments),
-						this.setState({ comments: '' })
-					)}
-				>
-					<Text>버튼</Text>
-				</TouchableOpacity>
-			</PostScreen>
-		);
-	}
+function PostName({ post, route }) {
+	console.log(post, '우우우ㅜㅇ');
+	return (
+		<View>
+			<Title> title {post[route.params.postIndex].title}</Title>
+			<PostNickname>닉네임 대전 {post[route.params.postIndex].user.nickname}</PostNickname>
+			<Time> 작성시간 {post[route.params.postIndex].updatedAt}</Time>
+			<Title>글 내용 {post[route.params.postIndex].contents}</Title>
+		</View>
+	);
+}
+const PostNames = React.memo(PostName);
+
+function Commentss({ post, route }) {
+	console.log(post, route);
+	return (
+		<View>
+			{post[route.params.postIndex].comments.map((comments: any, index) => (
+				<Text key={index}>{comments.Contents}</Text>
+			))}
+		</View>
+	);
+}
+const Commentsss = React.memo(Commentss);
+
+function PostDetail({ post, route, writingComment, userId }) {
+	const [writeComments, setComments] = useState('');
+	// console.log(post, route, writingComment, userId);
+	console.log(writeComments);
+
+	return (
+		<PostScreen>
+			<Post>
+				<PostNames post={post} route={route}></PostNames>
+				{/* <Title> title {post[route.params.postIndex].title}</Title>
+				<PostNickname>닉네임 대전 {post[route.params.postIndex].user.nickname}</PostNickname>
+				<Time> 작성시간 {post[route.params.postIndex].updatedAt}</Time>
+				<Title>글 내용 {post[route.params.postIndex].contents}</Title> */}
+				<TextIN>
+					<TextInput value={writeComments} onChangeText={(comments) => setComments(comments)}></TextInput>
+				</TextIN>
+			</Post>
+			<TouchableOpacity onPress={() => (writingComment(userId, route.params.postId, writeComments), setComments(''))}>
+				<Text>버튼</Text>
+			</TouchableOpacity>
+			<Commentsss post={post} route={route}></Commentsss>
+		</PostScreen>
+	);
 }
 
 export default PostDetail;
